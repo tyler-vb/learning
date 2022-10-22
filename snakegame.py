efe9e1a12cc11455095bacbe1a6186e4003d40ff
tyler-vb <tylerbuffa@gmail.com>
2022-10-22 01:12:42 -0700

    commiting

--- snakegame.py
+++ snakegame.py
@@ -11,29 +11,24 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
+# fonts
+game_font = pygame.font.SysFont('Comic Sans MS', 30)
+    
 # size of window
-width = 600
-length = 600
+width = 500
+length = 500
 
-# FPS
-fps = 10
+# snake speed
+max_snake_speed = 18
 
 # create game window
 dis = pygame.display.set_mode((width, length))
 pygame.display.set_caption("snake by Tyler Buffa")
 
 # initialize snake parameters
-block_size = 10
-x1 = 300
-y1 = 300
-x2 = random.randrange(0, width, block_size)
-y2 = random.randrange(0, length, block_size)
-x1_change = 0
-y1_change = 0
-segment_list = []
-snake_length = 1
+block_size = 25
 
-# speed of the game
+# initialize clock
 clock = pygame.time.Clock()
 
 # function to draw out each piece of snake
@@ -41,74 +36,124 @@ def snake(segment_list, segment_size):
     for x in segment_list:
         pygame.draw.rect(dis, black, [x[0], x[1], segment_size, segment_size])
 
-game_over = False
+def retry_game():
 
-while not game_over:
+        dis.fill(red)
+        text = game_font.render('Press C to Retry or Q to Quit', 1, white)
+        text_rect = text.get_rect(center = (width/2, length/2))
+        dis.blit(text, text_rect)
+        pygame.display.update()
 
-    # gameover if snake crosses boundry
-    if x1 >= width or x1 <= 0 or y1 >= length or y1 <= 0:   
-        game_over = True
+def game_loop():
+    
+    game_over = False
+    game_close = False
 
-    # get keystrokes
-    for event in pygame.event.get():
-        print(event)
+    x1 = width/2
+    y1 = length/2
+    x2 = random.randrange(0, width, block_size)
+    y2 = random.randrange(0, length, block_size)
+    x1_change = 0
+    y1_change = 0
+    segment_list = []
+    snake_length = 1
+    snake_speed = 10
 
-        # close window if press close button
-        if event.type == pygame.QUIT:
+    while not game_close:
+
+        keystroke = False
+
+        while game_over == True:
+                retry_game()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        game_over = False
+                        game_close = True
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_q:
+                            game_over = False
+                            game_close = True
+                        elif event.key == pygame.K_c:
+                            game_over = False
+                            game_loop()
+
+
+        # get keystrokes
+        for event in pygame.event.get():
+            print(event)
+
+            # close window if press close button
+            if event.type == pygame.QUIT:
+                game_close = True
+
+            # player movement
+            if event.type == pygame.KEYDOWN and keystroke == False:
+                if event.key == pygame.K_LEFT and x1_change == 0:
+                    x1_change = -block_size
+                    y1_change = 0
+                    keystroke = True
+                elif event.key == pygame.K_RIGHT and x1_change == 0:
+                    x1_change = block_size
+                    y1_change = 0
+                    keystroke = True
+                elif event.key == pygame.K_DOWN and y1_change == 0:
+                    x1_change = 0
+                    y1_change = block_size
+                    keystroke = True
+                elif event.key == pygame.K_UP and y1_change == 0:
+                    x1_change = 0
+                    y1_change = -block_size
+                    keystroke = True
+
+        # update snake head position
+        x1 += x1_change
+        y1 += y1_change
+
+        # add snake segment and increase snake speed if snake eats food
+        if x1 == x2 and y1 == y2:
+            on_snake = True
+            while on_snake:
+                x2 = random.randrange(0, width, block_size)
+                y2 = random.randrange(0, length, block_size)
+                if [x2, y2] not in segment_list:
+                    on_snake = False
+            if snake_speed < max_snake_speed:
+                snake_speed += 1
+            snake_length += 1
+
+        # clear the game space
+        dis.fill((white))
+
+        # update snake list with new head position
+        segment_list.append([x1, y1])
+
+        # remove old parts of snake from snake list
+        if len(segment_list) > snake_length:
+            del segment_list[0]
+
+        # draw out snake
+        snake(segment_list, block_size)
+
+        # draw out food
+        pygame.draw.rect(dis, red, [x2, y2, block_size, block_size])
+
+        # gameover if snake crosses self
+        if [x1, y1] in segment_list[:-1]:
             game_over = True
 
-        # player movement
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x1_change = -block_size
-                y1_change = 0
-            elif event.key == pygame.K_RIGHT:
-                x1_change = block_size
-                y1_change = 0
-            elif event.key == pygame.K_DOWN:
-                x1_change = 0
-                y1_change = block_size
-            elif event.key == pygame.K_UP:
-                x1_change = 0
-                y1_change = -block_size
+        # gameover if snake crosses boundry
+        if x1 >= width or x1 <= 0 or y1 >= length or y1 <= 0:   
+            game_over = True
+        
+        # update the screen
+        pygame.display.update()
 
-    # update snake head position
-    x1 += x1_change
-    y1 += y1_change
+        clock.tick(snake_speed)
+        
+    pygame.quit()
+    sys.exit()
 
-    if x1 == x2 and y1 == y2:
-        x2 = random.randrange(0, width, block_size)
-        y2 = random.randrange(0, length, block_size)
-        snake_length += 1
+game_loop()
 
-
-    snake_head = [x1, y1]
-
-    # clear the game space
-    dis.fill((white))
-
-    # update snake list with new head position
-    segment_list.append([x1, y1])
-
-    # remove old parts of snake from snake list
-    if len(segment_list) > snake_length:
-        del segment_list[0]
-
-    # draw out snake
-    snake(segment_list, block_size)
-
-    # draw out food
-    pygame.draw.rect(dis, red, [x2, y2, block_size, block_size])
-
-
-    print('snake', x1, y1)
-    print('food', x2, y2)
-    # update the screen
-    pygame.display.update()
-
-    clock.tick(fps)
-
-pygame.quit()
-sys.exit()
 
 
