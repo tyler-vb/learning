cdbbeb6ea8abc660519d274d9c3b59744f712acc
tyler-vb <tylerbuffa@gmail.com>
2022-10-22 22:19:36 -0700

    butt

--- snakegame.py
+++ snakegame.py
@@ -2,6 +2,7 @@
 import pygame
 import sys
 import random
+import json
 
 # initialize pygame
 pygame.init()
@@ -10,6 +11,7 @@ pygame.init()
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
+blue = (0, 0, 255)
 
 # fonts
 game_font = pygame.font.SysFont('Comic Sans MS', 30)
@@ -36,13 +38,17 @@ def snake(segment_list, segment_size):
     for x in segment_list:
         pygame.draw.rect(dis, black, [x[0], x[1], segment_size, segment_size])
 
-def retry_game():
-
+def retry_screen():
         dis.fill(red)
         text = game_font.render('Press C to Retry or Q to Quit', 1, white)
         text_rect = text.get_rect(center = (width/2, length/2))
         dis.blit(text, text_rect)
-        pygame.display.update()
+
+
+def score(a, b):
+        text = game_font.render(('score: ' + str(a) + ' | high score: ' + str(b)), 1, blue)
+        text_rect = text.get_rect(center = (width/2, length/10))
+        dis.blit(text, text_rect)
 
 def game_loop():
     
@@ -59,12 +65,26 @@ def game_loop():
     snake_length = 1
     snake_speed = 10
 
+    with open('snake.json', 'r') as openfile:
+        json_data = json.load(openfile)
+
+    high_score = json_data['highscore']
+
     while not game_close:
+
+        if high_score < snake_length - 1:
+
+            high_score += 1
+
+            json_data['highscore'] = high_score
+            with open('snake.json', 'w') as outfile:
+                json.dump(json_data, outfile)
 
         keystroke = False
 
         while game_over == True:
-                retry_game()
+                retry_screen()
+                pygame.display.update()
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         game_over = False
@@ -136,6 +156,9 @@ def game_loop():
 
         # draw out food
         pygame.draw.rect(dis, red, [x2, y2, block_size, block_size])
+
+        # score
+        score(snake_length - 1, high_score)
 
         # gameover if snake crosses self
         if [x1, y1] in segment_list[:-1]:
